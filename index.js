087944b460f5f732d2e7eb608ee1aba161249a73
Dimitar Mirchev <dirchev@camplight.net>
2019-02-16 18:27:43 +0000

    add local router

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const execute = async () => {
   app.use(bodyParser.json())
 
   let models = loadModels()
-  await loadRoutes({app, models})
+  let router = await loadRoutes({models})
+  app.use('/api', router)
 
   const port = 8080
   app.listen(port, () => console.log(`Web Service listening on port ${port}!`))
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,52 @@
 const loadRouteConstructor = require('./loadRouteConstructor')
+const Router = require('express').Router
+const KNOWN_HOSTNAMES = process.env.hostname
+let getQuizAppFromHostname = function (hostname) {
 
-let routes = async function ({app, models = {}}) {
-  let apiHelpers = require('./api-helpers')(models)
-
-  // API endpoints
-  let loadRoute = loadRouteConstructor(app, {models, apiHelpers})
-  loadRoute('post', '/api/login', require('./api/login'))
-  loadRoute('post', '/api/register', require('./api/register'))
-  loadRoute('post', '/api/logout', require('./api/logout'))
-
-  // QUIZ APP
-  loadRoute('get', '/api/quiz-app/:quizAppId', require('./api/quiz-app/retrieve'))
-  loadRoute('get', '/api/quiz-app/', require('./api/quiz-app/list'))
-  loadRoute('post', '/api/quiz-app', require('./api/quiz-app/create'))
-  loadRoute('put', '/api/quiz-app/:quizAppId', require('./api/quiz-app/update'))
-  loadRoute('delete', '/api/quiz-app/:quizAppId', require('./api/quiz-app/delete'))
-
-  loadRoute('get', '/api/quiz-app/:quizAppId/teachers', require('./api/teachers/list'))
-  loadRoute('post', '/api/quiz-app/:quizAppId/teachers', require('./api/teachers/add-teacher'))
-  loadRoute('delete', '/api/quiz-app/:quizAppId/teachers/:teacherId', require('./api/teachers/remove-teacher'))
-
-  loadRoute('get', '/api/quiz-app/:quizAppId/quizess', require('./api/quizess/list'))
-  loadRoute('get', '/api/quiz-app/:quizAppId/quizess/:quizId', require('./api/quizess/retrieve'))
-  loadRoute('post', '/api/quiz-app/:quizAppId/quizess', require('./api/quizess/create'))
-  loadRoute('put', '/api/quiz-app/:quizAppId/quizess/:quizId', require('./api/quizess/update'))
-  loadRoute('delete', '/api/quiz-app/:quizAppId/quizess/:quizId', require('./api/quizess/delete'))
-  loadRoute('post', '/api/quiz-app/:quizAppId/quizess/:quizId/publish', require('./api/quizess/publish'))
 }
 
-module.exports = routes
+let loadRoutes = async function ({models = {}}) {
+  let apiHelpers = require('./api-helpers')(models)
+  let router = new Router()
+
+  router.use((req, res, next) => {
+    let quizAppName = getQuizAppFromHostname(req.hostname)
+    if (!quizAppName) return next()
+
+  })
+
+  router.param('quizAppId', (req, res, next, id) => {
+    models.Application.findById(id, function (err, quizApp) {
+      if (err) return next(err)
+      req.quizApp = quizApp
+      next()
+    })
+  })
+
+  // API endpoints
+  let loadRoute = loadRouteConstructor(router, {models, apiHelpers})
+  loadRoute('post', '/login', require('./api/login'))
+  loadRoute('post', '/register', require('./api/register'))
+  loadRoute('post', '/logout', require('./api/logout'))
+
+  // QUIZ APP
+  loadRoute('get', '/quiz-app/:quizAppId', require('./api/quiz-app/retrieve'))
+  loadRoute('get', '/quiz-app/', require('./api/quiz-app/list'))
+  loadRoute('post', '/quiz-app', require('./api/quiz-app/create'))
+  loadRoute('put', '/quiz-app/:quizAppId', require('./api/quiz-app/update'))
+  loadRoute('delete', '/quiz-app/:quizAppId', require('./api/quiz-app/delete'))
+
+  loadRoute('get', '/quiz-app/:quizAppId/teachers', require('./api/teachers/list'))
+  loadRoute('post', '/quiz-app/:quizAppId/teachers', require('./api/teachers/add-teacher'))
+  loadRoute('delete', '/quiz-app/:quizAppId/teachers/:teacherId', require('./api/teachers/remove-teacher'))
+  loadRoute('get', '/quiz-app/:quizAppId/quizess', require('./api/quizess/list'))
+  loadRoute('get', '/quiz-app/:quizAppId/quizess/:quizId', require('./api/quizess/retrieve'))
+  loadRoute('post', '/quiz-app/:quizAppId/quizess', require('./api/quizess/create'))
+  loadRoute('put', '/quiz-app/:quizAppId/quizess/:quizId', require('./api/quizess/update'))
+  loadRoute('delete', '/quiz-app/:quizAppId/quizess/:quizId', require('./api/quizess/delete'))
+  loadRoute('post', '/quiz-app/:quizAppId/quizess/:quizId/publish', require('./api/quizess/publish'))
+
+  return router
+}
+
+module.exports = loadRoutes
