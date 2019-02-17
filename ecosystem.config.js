47e86ced31827653caf25dbf1780bea007202a54
Dimitar Mirchev <dirchev@camplight.net>
2019-02-17 21:29:54 +0000

    add .env and ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps: [{
+    name: "main",
+    script: "index.js",
+    merge_logs: true
+  }],
+  deploy: {
+    // VMWARE local machine
+    local: {
+      // SSH key path, default to $HOME/.ssh
+      key: "$HOME/.ssh",
+      // SSH user
+      user: "dirchev",
+      // SSH host
+      host: ["quiz-app.loc"],
+      // ssh_options: "StrictHostKeyChecking=no",
+      ref: "origin/master",
+      // GIT remote
+      repo: "git@github.com:dirchev/quiz-app-server.git",
+      // path in the server
+      path: "/home/dirchev/quiz-app-server",
+      // Pre-setup command or path to a script on your local machine
+      "pre-setup": "apt-get install git ; ls -la",
+      // Post-setup commands or path to a script on the host machine
+      // eg: placing configurations in the shared dir etc
+      "post-setup": "ls -la",
+      // pre-deploy action
+      "pre-deploy-local": "echo 'This is a local executed command'",
+      // post-deploy action
+      "post-deploy": "npm install",
+    },
+  }
+}
