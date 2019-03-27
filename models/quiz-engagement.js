34d7b089b9e4c0d897454ca128f4adb26dc4ffa4
Dimitar Mirchev <dirchev@camplight.net>
2019-03-27 20:03:30 +0000

    implement calculateMarksForQuizEngagement methods

--- models/quiz-engagement.js
+++ models/quiz-engagement.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const ObjectId = mongoose.Schema.Types.ObjectId
 const Mixed = mongoose.Schema.Types.Mixed
+const {xor, find} = require('lodash')
 
 let schema = new mongoose.Schema({
   quizApp: {type: ObjectId, ref: 'Application', required: true},
@@ -56,6 +57,49 @@ schema.pre('validate', async function () {
   if (existingEngagementsCount >= noOfAttempts) {
     this.invalidate('student', 'Number of attempts are reached for this quiz.')
   }
+})
+
+let getPointsForQuestionAnswer = function (question, answerGiven) {
+  switch (question.type) {
+    case 'MCQ_ONE_RIGHT':
+      return question.answers.correctId === answerGiven
+        ? question.points
+        : 0
+    case 'MCQ_MULTIPLE_RIGHT':
+      let idsOfCorrectAnswers = question.answers.options.filter(({isCorrect}) => isCorrect).map(({_id}) => _id)
+      return xor(idsOfCorrectAnswers, answerGiven).length === 0
+        ? question.points
+        : 0
+    case 'FREE_SHORT_TEXT':
+      return answerGiven === question.correctAnswer
+        ? question.points
+        : 0
+    default:
+      return 0
+  }
+}
+
+schema.static('calculateMarksForQuizEngagement', async function (quizEngagementId) {
+  let quizEngagement = await mongoose.model('QuizEngagement').findById(quizEngagementId, '+answersMarks +answersFeedbacks')
+  let quiz = await mongoose.model('Quiz').findById(quizEngagement.quiz)
+  await quizEngagement.calculateMarks()
+  if (quiz.canMarkAutomatically) {
+    quizEngagement.marked = true
+    quizEngagement.markedAt = new Date
+  }
+  return quizEngagement.save()
+})
+
+schema.method('calculateMarks', async function () {
+  let quiz = await mongoose.model('Quiz').findById(this.quiz)
+  this.set('answersMarks', new Map())
+  this.set('answersFeedbacks', new Map())
+  Object.keys(this.toJSON().answersGiven).forEach((questionId) => {
+    let question = find(quiz.questions, ({_id}) => _id.toString() === questionId)
+    let points = getPointsForQuestionAnswer(question, this.answersGiven[questionId])
+    this.answersMarks.set(questionId, points)
+    this.answersFeedbacks.set(questionId, question.defaultFeedback)
+  })
 })
 
 let QuizEngagement = mongoose.model('QuizEngagement', schema)
