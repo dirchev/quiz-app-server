4ae89ba8941339602b4147e99d1e18a2e6fb20e5
Dimitar Mirchev <dirchev@camplight.net>
2019-04-09 09:26:56 +0100

    add log

--- routes/api/user-tests/finish.js
+++ routes/api/user-tests/finish.js
@@ -4,6 +4,7 @@ module.exports = function ({models, apiHelpers}) {
       let userTest = await models.UserTest.findById(req.params.userTestId)
       userTest.addStep('Finish', req.body.date)
       await userTest.save()
+      console.log(JSON.stringify(userTest.toJSON(), null, 2))
       res.body = userTest
     }
   ]
